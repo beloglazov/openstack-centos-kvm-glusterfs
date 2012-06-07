d8c51251e06d879d8afd4389bbff2ced42f9438a
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-07 15:26:16 +1000

    Fixed mounting from ibm1 to localhost

--- 12-gluster-mount.sh
+++ 12-gluster-mount.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 mkdir /kvm-storage
-echo "ibm1:/kvm-storage /kvm-storage glusterfs defaults 0 0" >> /etc/fstab
+echo "localhost:/kvm-storage /kvm-storage glusterfs defaults 0 0" >> /etc/fstab
 mount -a
