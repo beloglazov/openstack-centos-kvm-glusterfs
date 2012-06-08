75c48411d482b3926197a62018d0ce9ee2fa4c85
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-08 16:08:01 +1000

    Downloaded another image

---
+++ 20-2-create-vm.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+virt-install --connect=qemu:///system --name=fedora-test --ram=1512 --vcpus=2 --disk /kvm-storage/f16-x86_64-openstack-sda.qcow2,format=qcow2 --import
--- 20-create-vm.sh
+++ 20-create-vm.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-virt-install --connect=qemu:///system --name=ubuntu-test --ram=512 --vcpus=1 --disk=/kvm-storage/precise-server-cloudimg-amd64-disk1.img --import
+virt-install --connect=qemu:///system --name=ubuntu-test --ram=1512 --vcpus=2 --disk /kvm-storage/precise-server-cloudimg-amd64-disk1.img,format=qcow2 --import
