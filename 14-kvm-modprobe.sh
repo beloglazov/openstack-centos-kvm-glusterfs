9f1fde68c52f15e7578a2d46d0d3ed315d07e100
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-08 13:45:03 +1000

    Added a comment for kvm-amd

--- 14-kvm-modprobe.sh
+++ 14-kvm-modprobe.sh
@@ -2,6 +2,7 @@
 
 echo "#!/bin/sh
 modprobe kvm
+#modprobe kvm-amd
 modprobe kvm-intel" > /etc/sysconfig/modules/kvm.modules
 chmod +x /etc/sysconfig/modules/kvm.modules
 /etc/sysconfig/modules/kvm.modules
