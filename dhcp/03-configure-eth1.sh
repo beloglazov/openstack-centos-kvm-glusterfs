81925320fc4238f2016bdcd87c90b385bebca033
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-13 11:59:28 +1000

    Fixed the dhcp scripts, added setting up the hosts file on the clients

---
+++ dhcp/03-configure-eth1.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp ifcfg-eth1 /etc/sysconfig/network-scripts/ifcfg-eth1
+cp hosts /etc/hosts
---
+++ dhcp/05-install-hosts.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+scp hosts compute2:/etc/hosts
+scp hosts compute3:/etc/hosts
+#scp hosts compute4:/etc/hosts
+scp hosts controller:/etc/hosts
