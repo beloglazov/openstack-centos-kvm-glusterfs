90c95503912601c3727cf755976803e0c5a042f0
root <root@compute2.(none)>
2012-06-14 14:46:56 +1000

    Added migration ports to the first iptables script

--- 01-1-gluster-iptables.sh
+++ 01-1-gluster-iptables.sh
@@ -7,7 +7,7 @@ iptables -t mangle -F
 iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 24007:24047 -j ACCEPT
 iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 111 -j ACCEPT
 iptables -A INPUT -m state --state NEW -m udp -p udp --dport 111 -j ACCEPT
+iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 49152:49216 -j ACCEPT
 
 service iptables save
 service iptables restart
-
--- 26-iptables-for-migration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 49152:49216 -j ACCEPT
-
-service iptables save
-service iptables restart
-
