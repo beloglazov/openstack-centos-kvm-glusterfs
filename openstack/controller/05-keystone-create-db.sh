d5c741d0de2af8181a79fcc8ff2e43fff3f9e4f0
root <root@controller.(none)>
2012-06-18 13:51:22 +1000

    Added keystone admin token generator

--- openstack/controller/05-keystone-create-db.sh
+++ openstack/controller/05-keystone-create-db.sh
@@ -2,5 +2,4 @@
 
 ./mysqlq.sh "CREATE DATABASE keystone;"
 ./mysqlq.sh "GRANT ALL ON keystone.* TO 'keystone'@'%' IDENTIFIED BY '$MYSQL_KEYSTONE_PASSWORD';"
-#./mysqlq.sh "CREATE USER 'keystone'@'%' IDENTIFIED BY '$MYSQL_KEYSTONE_PASSWORD';"
 
