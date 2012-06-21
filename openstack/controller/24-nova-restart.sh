8bd587a619e9cb0416680098f792aa6bc869ff96
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-21 18:38:19 +1000

    Added a script for restarting nova, added a readme on how to authenticate nova in keystone

---
+++ openstack/controller/24-nova-restart.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+service openstack-nova-api stop
+service openstack-nova-network stop
+service openstack-nova-scheduler stop
+service openstack-nova-xvpvncproxy stop
+service openstack-nova-volume stop
+service openstack-nova-cert stop
+service openstack-nova-consoleauth stop
+service openstack-nova-console stop
+
+nova-manage db sync
+
+service openstack-nova-api start
+service openstack-nova-network start
+service openstack-nova-scheduler start
+service openstack-nova-xvpvncproxy start
+service openstack-nova-volume start
+service openstack-nova-cert start
+service openstack-nova-consoleauth start
+service openstack-nova-console start
