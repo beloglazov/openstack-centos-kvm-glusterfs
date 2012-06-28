e427ab66beb725c1ef14f0f4b1f7fb8a3e9b2add
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-28 17:56:55 +1000

    Changed % to controller for glance and nova as well

--- openstack/controller/13-glance-create-db.sh
+++ openstack/controller/13-glance-create-db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ./mysqlq.sh "CREATE DATABASE glance;"
-./mysqlq.sh "GRANT ALL ON glance.* TO 'glance'@'%' IDENTIFIED BY '$MYSQL_GLANCE_PASSWORD';"
+./mysqlq.sh "GRANT ALL ON glance.* TO 'glance'@'controller' IDENTIFIED BY '$MYSQL_GLANCE_PASSWORD';"
 
