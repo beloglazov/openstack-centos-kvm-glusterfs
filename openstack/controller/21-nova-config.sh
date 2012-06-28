05b7c5acdb8e5712c0c977e4390b5917a14a60fa
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-28 14:03:37 +1000

    Added qpid_hostname controller

--- openstack/controller/21-nova-config.sh
+++ openstack/controller/21-nova-config.sh
@@ -2,3 +2,4 @@
 
 openstack-config --set /etc/nova/nova.conf DEFAULT sql_connection mysql://nova:$MYSQL_NOVA_PASSWORD@controller/nova
 openstack-config --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
+openstack-config --set /etc/nova/nova.conf DEFAULT qpid_hostname controller
