fd98ebca61298fa1e81581338a3af877318bf93d
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-28 18:37:36 +1000

    Changed the default tenant from regular-user to admin

--- openstack/controller/11-keystone-create-users.sh
+++ openstack/controller/11-keystone-create-users.sh
@@ -4,7 +4,7 @@ git clone https://github.com/nimbis/keystone-init.git
 
 sed -i "s/192.168.206.130/controller/g" keystone-init/config.yaml
 sed -i "s/012345SECRET99TOKEN012345/`cat keystone-admin-token`/g" keystone-init/config.yaml
-sed -i "s/openstackDemo/regular-user/g" keystone-init/config.yaml
+sed -i "s/openstackDemo/$OS_TENANT_NAME/g" keystone-init/config.yaml
 sed -i "s/adminUser/$OS_USERNAME/g" keystone-init/config.yaml
 sed -i "s/secretword/$OS_PASSWORD/g" keystone-init/config.yaml
 sed -i "s/password: glance/password: $OS_SERVICE_PASSWORD/g" keystone-init/config.yaml
@@ -14,4 +14,5 @@ sed -i "s/password: swiftpass/password: $OS_SERVICE_PASSWORD/g" keystone-init/co
 
 ./keystone-init/keystone-init.py ./keystone-init/config.yaml
 
-#rm -rf keystone-init
+echo "The applied config file is keystone-init/config.yaml"
+echo "Do 'rm -rf keystone-init' to remove the no more needed keystone-init directory"
