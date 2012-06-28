5024145c78200bf0674fba3a6d8731a0cbed4564
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-28 15:49:52 +1000

    Fixed the create network script

--- openstack/bridge/03-nova-config.sh
+++ openstack/bridge/03-nova-config.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+openstack-config --set /etc/nova/nova.conf DEFAULT verbose True
 openstack-config --set /etc/nova/nova.conf DEFAULT sql_connection mysql://nova:$MYSQL_NOVA_PASSWORD@controller/nova
 openstack-config --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
 openstack-config --set /etc/nova/nova.conf DEFAULT network_host compute1
 openstack-config --set /etc/nova/nova.conf DEFAULT fixed_range 192.168.0.0/24
 openstack-config --set /etc/nova/nova.conf DEFAULT flat_network_dhcp_start 192.168.0.20
 openstack-config --set /etc/nova/nova.conf DEFAULT flat_interface eth1
+openstack-config --set /etc/nova/nova.conf DEFAULT flat_network_bridge br100
 openstack-config --set /etc/nova/nova.conf DEFAULT public_interface eth0
 openstack-config --set /etc/nova/nova.conf DEFAULT qpid_hostname controller
--- openstack/bridge/08-nova-create-network.sh
+++ openstack/bridge/08-nova-create-network.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-nova-manage network create public 192.168.0.0/24 20 255
+nova-manage network create --label=public --fixed_range_v4=192.168.0.0/24 --num_networks=1 --network_size=256 --bridge=br100
