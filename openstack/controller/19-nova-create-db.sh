b379e54f09f1e04b31ff62ac81c75dddc4cbbf57
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-29 14:54:02 +1000

    Fixed nova create db

--- openstack/controller/19-nova-create-db.sh
+++ openstack/controller/19-nova-create-db.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 ./mysqlq.sh "CREATE DATABASE nova;"
-./mysqlq.sh "GRANT ALL ON nova.* TO 'nova'@'controller' IDENTIFIED BY '$MYSQL_NOVA_PASSWORD';"
+./mysqlq.sh "GRANT ALL ON nova.* TO 'nova'@'controller' IDENTIFIED BY '$NOVA_MYSQL_PASSWORD';"
