d6bd5c4998d3cebf5246e5884c4d7265a5bacaea
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-29 15:45:16 +1000

    Added another entry for the nova mysql user to allow access from remote hosts

--- openstack/controller/19-nova-create-db.sh
+++ openstack/controller/19-nova-create-db.sh
@@ -2,3 +2,6 @@
 
 ./mysqlq.sh "CREATE DATABASE nova;"
 ./mysqlq.sh "GRANT ALL ON nova.* TO 'nova'@'controller' IDENTIFIED BY '$NOVA_MYSQL_PASSWORD';"
+
+# The following is need to allow access from nova services running on other hosts
+./mysqlq.sh "GRANT ALL ON nova.* TO 'nova'@'%' IDENTIFIED BY '$NOVA_MYSQL_PASSWORD';"
