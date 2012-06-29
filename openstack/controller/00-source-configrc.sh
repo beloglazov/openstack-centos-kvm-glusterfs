660402e4b3d8687a3a0bf49ed2d477e46e4585a4
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-29 11:42:10 +1000

    Renamed: moved source configrc and mysql to the beginning

---
+++ openstack/controller/00-source-configrc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+echo "To make the environmental variable available in your session, run: "
+echo ". ./00-source-configrc.sh"
+. ../config/configrc
--- openstack/controller/04-keystone-install.sh
+++ openstack/controller/04-keystone-install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
 yum install -y openstack-utils openstack-keystone
-
-rm /var/lib/keystone/keystone.db
