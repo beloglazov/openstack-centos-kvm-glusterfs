fa52140c2eac538ce4ac8af53dc94588e68c2311
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-06-29 12:39:08 +1000

    Fixed keystone-create-db

--- openstack/controller/05-keystone-create-db.sh
+++ openstack/controller/05-keystone-create-db.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ./mysqlq.sh "CREATE DATABASE keystone;"
-./mysqlq.sh "GRANT ALL ON keystone.* TO 'keystone'@'controller' IDENTIFIED BY '$MYSQL_KEYSTONE_PASSWORD';"
+./mysqlq.sh "GRANT ALL ON keystone.* TO 'keystone'@'controller' IDENTIFIED BY '$KEYSTONE_MYSQL_PASSWORD';"
 
