cd99aadd896c39988f3df62e1de475092ab9dc8f
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-04 18:12:56 +1000

    Removed unnecessary comment from the nova-permissions script

--- openstack/compute/06-nova-permissions.sh
+++ openstack/compute/06-nova-permissions.sh
@@ -9,6 +9,3 @@ chown -R nova:nova /var/lib/libvirt
 
 sed -i 's/#user = "root"/user = "nova"/g' /etc/libvirt/qemu.conf
 sed -i 's/#group = "root"/group = "nova"/g' /etc/libvirt/qemu.conf
-
-# if VM creation still fails, need to do:
-#usermod -a -G nova qemu
