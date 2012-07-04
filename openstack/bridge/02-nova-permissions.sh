f54e20a147fa643607788939bc7afe16552525b4
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-04 16:36:15 +1000

    Fixed the network configuration for the bridge

--- openstack/bridge/02-nova-permissions.sh
+++ openstack/bridge/02-nova-permissions.sh
@@ -2,3 +2,10 @@
 
 chown -R root:nova /etc/nova
 chmod 640 /etc/nova/nova.conf
+
+chown -R nova:nova /var/cache/libvirt
+chown -R nova:nova /var/run/libvirt
+chown -R nova:nova /var/lib/libvirt
+
+sed -i 's/#user = "root"/user = "nova"/g' /etc/libvirt/qemu.conf
+sed -i 's/#group = "root"/user = "nova"/g' /etc/libvirt/qemu.conf
