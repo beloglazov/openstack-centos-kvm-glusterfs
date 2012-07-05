069a36d5d89de45138e701639ab34db310ba08e1
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-05 10:47:36 +1000

    Added VNC proxy configuration

--- openstack/controller/21-nova-config.sh
+++ openstack/controller/21-nova-config.sh
@@ -8,3 +8,11 @@ openstack-config --set /etc/nova/api-paste.ini filter:authtoken admin_tenant_nam
 openstack-config --set /etc/nova/api-paste.ini filter:authtoken admin_user $NOVA_SERVICE_USERNAME
 openstack-config --set /etc/nova/api-paste.ini filter:authtoken admin_password $NOVA_SERVICE_PASSWORD
 openstack-config --set /etc/nova/api-paste.ini filter:authtoken auth_uri $NOVA_OS_AUTH_URL
+
+# VNC setup
+openstack-config --set /etc/nova/nova.conf DEFAULT vncserver_listen 0.0.0.0
+openstack-config --set /etc/nova/nova.conf DEFAULT vncserver_proxyclient_address controller
+# This is the public IP accessible from outside, where novncproxy is running on
+openstack-config --set /etc/nova/nova.conf DEFAULT novncproxy_base_url 128.250.26.161
+# This is the public IP accessible from outside, where novncproxy is running on
+openstack-config --set /etc/nova/nova.conf DEFAULT xvpvncproxy_base_url 128.250.26.161
