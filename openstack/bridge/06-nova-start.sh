d731b469f6a056fbec0d28bfe3b66fa0d0c9ddc2
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-05 10:58:26 +1000

    Added starting vnc proxies

--- openstack/bridge/06-nova-start.sh
+++ openstack/bridge/06-nova-start.sh
@@ -3,7 +3,10 @@
 service libvirtd restart
 service openstack-nova-network restart
 service openstack-nova-compute restart
+service openstack-nova-novncproxy restart
+service openstack-nova-xvpvncproxy restart
 
 chkconfig openstack-nova-network on
 chkconfig openstack-nova-compute on
-
+chkconfig openstack-nova-novncproxy on
+chkconfig openstack-nova-xvpvncproxy on
