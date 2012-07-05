b6d8ac96f88ec3197a5b83c3135c1be03e4a3d8e
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-05 10:53:55 +1000

    Synced nova config scripts

--- openstack/controller/21-nova-config.sh
+++ openstack/controller/21-nova-config.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+openstack-config --set /etc/nova/nova.conf DEFAULT verbose True
+
 openstack-config --set /etc/nova/nova.conf DEFAULT sql_connection mysql://nova:$NOVA_MYSQL_PASSWORD@controller/nova
 openstack-config --set /etc/nova/nova.conf DEFAULT auth_strategy keystone
 openstack-config --set /etc/nova/nova.conf DEFAULT qpid_hostname controller
@@ -8,6 +10,21 @@ openstack-config --set /etc/nova/api-paste.ini filter:authtoken admin_tenant_nam
 openstack-config --set /etc/nova/api-paste.ini filter:authtoken admin_user $NOVA_SERVICE_USERNAME
 openstack-config --set /etc/nova/api-paste.ini filter:authtoken admin_password $NOVA_SERVICE_PASSWORD
 openstack-config --set /etc/nova/api-paste.ini filter:authtoken auth_uri $NOVA_OS_AUTH_URL
+
+# may also need to set service_host and auth_host to controller
+
+
+# Network setup
+openstack-config --set /etc/nova/nova.conf DEFAULT network_host compute1
+openstack-config --set /etc/nova/nova.conf DEFAULT fixed_range 10.0.0.0/24
+openstack-config --set /etc/nova/nova.conf DEFAULT flat_interface eth1
+openstack-config --set /etc/nova/nova.conf DEFAULT flat_network_bridge br100
+openstack-config --set /etc/nova/nova.conf DEFAULT public_interface eth0
+
+
+# Glance setup
+openstack-config --set /etc/nova/nova.conf DEFAULT glance_host controller
+
 
 # VNC setup
 openstack-config --set /etc/nova/nova.conf DEFAULT vncserver_listen 0.0.0.0
