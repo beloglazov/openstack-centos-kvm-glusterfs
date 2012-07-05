0fa47b2937ff8c3e0df91dfbc9bfdd7c900c5b50
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-05 11:25:56 +1000

    Enabled nova-consoleauth on the controller

--- openstack/controller/23-nova-start.sh
+++ openstack/controller/23-nova-start.sh
@@ -3,11 +3,11 @@
 service qpidd restart
 service openstack-nova-api restart
 service openstack-nova-scheduler restart
-service openstack-nova-xvpvncproxy restart
 service openstack-nova-volume restart
 service openstack-nova-cert restart
+service openstack-nova-consoleauth restart
 # This service is used by VNC client proxies
-#service openstack-nova-consoleauth restart
+#service openstack-nova-xvpvncproxy restart
 #service openstack-nova-novncproxy restart
 # This is a Xen specific service
 #service openstack-nova-console restart
@@ -15,10 +15,10 @@ service openstack-nova-cert restart
 chkconfig qpidd on
 chkconfig openstack-nova-api on
 chkconfig openstack-nova-scheduler on
-chkconfig openstack-nova-xvpvncproxy on
 chkconfig openstack-nova-volume on
 chkconfig openstack-nova-cert on
-#chkconfig openstack-nova-consoleauth on
+chkconfig openstack-nova-consoleauth on
 #chkconfig openstack-nova-novncproxy on
+#chkconfig openstack-nova-xvpvncproxy on
 #chkconfig openstack-nova-console on
 
