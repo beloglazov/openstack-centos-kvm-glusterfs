e64c89c6e6824ac6e011881144a3b7d1f5819d3e
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-05 15:16:29 +1000

    Added direct-api and metadata-api service to the controller

--- openstack/controller/23-nova-start.sh
+++ openstack/controller/23-nova-start.sh
@@ -2,10 +2,12 @@
 
 service qpidd restart
 service openstack-nova-api restart
-service openstack-nova-scheduler restart
-service openstack-nova-volume restart
 service openstack-nova-cert restart
 service openstack-nova-consoleauth restart
+service openstack-nova-direct-api restart
+service openstack-nova-metadata-api restart
+service openstack-nova-scheduler restart
+service openstack-nova-volume restart
 # This service is used by VNC client proxies
 #service openstack-nova-xvpvncproxy restart
 #service openstack-nova-novncproxy restart
@@ -14,10 +16,12 @@ service openstack-nova-consoleauth restart
 
 chkconfig qpidd on
 chkconfig openstack-nova-api on
-chkconfig openstack-nova-scheduler on
-chkconfig openstack-nova-volume on
 chkconfig openstack-nova-cert on
 chkconfig openstack-nova-consoleauth on
+chkconfig openstack-nova-direct-api on
+chkconfig openstack-nova-metadata-api on
+chkconfig openstack-nova-scheduler on
+chkconfig openstack-nova-volume on
 #chkconfig openstack-nova-novncproxy on
 #chkconfig openstack-nova-xvpvncproxy on
 #chkconfig openstack-nova-console on
