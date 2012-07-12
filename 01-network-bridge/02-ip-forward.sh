f3a767d8d8a38126f50a68679a4e282856b972ea
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-13 09:47:58 +1000

    Renamed a couple of scripts, fixed comments

--- 01-network-bridge/02-ip-forward.sh
+++ 01-network-bridge/02-ip-forward.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
+# Enable IP forwarding
 sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/sysctl.conf 
 
 service network restart
-
-# TODO: update /etc/sysconfig/network-scripts/ifcfg-eth0 and etc.
