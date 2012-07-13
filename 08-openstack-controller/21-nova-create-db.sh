9697347fde432e25abb4417c1b141752ac188302
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-13 10:20:16 +1000

    Fixed the path to mysql.sh

--- 08-openstack-controller/21-nova-create-db.sh
+++ 08-openstack-controller/21-nova-create-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-./mysqlq.sh "CREATE DATABASE nova;"
-./mysqlq.sh "GRANT ALL ON nova.* TO 'nova'@'controller' IDENTIFIED BY '$NOVA_MYSQL_PASSWORD';"
+../lib/mysqlq.sh "CREATE DATABASE nova;"
+../lib/mysqlq.sh "GRANT ALL ON nova.* TO 'nova'@'controller' IDENTIFIED BY '$NOVA_MYSQL_PASSWORD';"
 
 # The following is need to allow access from nova services running on other hosts
-./mysqlq.sh "GRANT ALL ON nova.* TO 'nova'@'%' IDENTIFIED BY '$NOVA_MYSQL_PASSWORD';"
+../lib/mysqlq.sh "GRANT ALL ON nova.* TO 'nova'@'%' IDENTIFIED BY '$NOVA_MYSQL_PASSWORD';"
