31391800f1c8da124fbffd642dc5b92087126785
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-13 14:56:18 +1000

    Fixed openstack network interface configuration (eth0 -> eth1)

--- 10-openstack-gateway/02-nova-config.sh
+++ 10-openstack-gateway/02-nova-config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-openstack-config --set /etc/nova/nova.conf DEFAULT flat_interface eth1
+openstack-config --set /etc/nova/nova.conf DEFAULT public_interface eth0
--- lib/nova-config.sh
+++ lib/nova-config.sh
@@ -17,9 +17,9 @@ openstack-config --set /etc/nova/api-paste.ini filter:authtoken auth_uri $NOVA_O
 # Network setup
 openstack-config --set /etc/nova/nova.conf DEFAULT network_host compute1
 openstack-config --set /etc/nova/nova.conf DEFAULT fixed_range 10.0.0.0/24
-openstack-config --set /etc/nova/nova.conf DEFAULT flat_interface eth0
+openstack-config --set /etc/nova/nova.conf DEFAULT flat_interface eth1
 openstack-config --set /etc/nova/nova.conf DEFAULT flat_network_bridge br100
-openstack-config --set /etc/nova/nova.conf DEFAULT public_interface eth0
+openstack-config --set /etc/nova/nova.conf DEFAULT public_interface eth1
 
 
 # Glance setup
