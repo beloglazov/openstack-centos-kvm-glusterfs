a62e632d5dc9a33342c1804eb4f1d61ffc2740c9
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-16 14:50:35 +1000

    Added a script configuring libvirt for live migration

---
+++ 05-kvm-compute/04-libvirt-config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This configuration is required to enable live migration through OpenStack
+
+sed -i 's/#listen_tls = 0/listen_tls = 0/g' /etc/libvirt/libvirtd.conf
+sed -i 's/#listen_tcp = 1/listen_tcp = 1/g' /etc/libvirt/libvirtd.conf
+sed -i 's/#auth_tcp = "sasl"/auth_tcp = "none"/g' /etc/libvirt/libvirtd.conf
+
+sed -i 's/#LIBVIRTD_ARGS="--listen"/LIBVIRTD_ARGS="--listen"/g' /etc/libvirt/libvirtd.conf
