72bf9de4c6499b9413a2af4aef72a047b61a7bc7
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-16 14:51:38 +1000

    Fixed the libvirt-config script

--- 05-kvm-compute/04-libvirt-config.sh
+++ 05-kvm-compute/04-libvirt-config.sh
@@ -6,4 +6,4 @@ sed -i 's/#listen_tls = 0/listen_tls = 0/g' /etc/libvirt/libvirtd.conf
 sed -i 's/#listen_tcp = 1/listen_tcp = 1/g' /etc/libvirt/libvirtd.conf
 sed -i 's/#auth_tcp = "sasl"/auth_tcp = "none"/g' /etc/libvirt/libvirtd.conf
 
-sed -i 's/#LIBVIRTD_ARGS="--listen"/LIBVIRTD_ARGS="--listen"/g' /etc/libvirt/libvirtd.conf
+sed -i 's/#LIBVIRTD_ARGS="--listen"/LIBVIRTD_ARGS="--listen"/g' /etc/sysconfig/libvirtd
