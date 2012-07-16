0a22e396bed800efa42df6f89dee25b05b3b3788
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-16 19:02:14 +1000

    Fixed the license prepending script

--- lib/prepend-license-header-sh.sh
+++ lib/prepend-license-header-sh.sh
@@ -4,6 +4,13 @@
 # license-header file to the file specified as an argument if the
 # header has not yet been prepended.
 
+if [ $# -ne 1 ]
+then
+    echo "You must specify one argument - the file to prepend the license header to"
+    exit 1
+fi
+
+
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SOURCE=${DIR}/license-header-sh
 
@@ -15,17 +22,8 @@ SOURCE_LINE1=`cat $SOURCE | sed '1!d;q'`
 SOURCE_LINE2=`cat $SOURCE | sed '2!d;q'`
 SOURCE_LINE3=`cat $SOURCE | sed '3!d;q'`
 
-echo $TARGET_LINE1
-echo $TARGET_LINE2
-echo $TARGET_LINE3
-
-echo $SOURCE_LINE1
-echo $SOURCE_LINE2
-echo $SOURCE_LINE3
-
-if [ $TARGET_LINE1 != $SOURCE_LINE1 -o $TARGET_LINE2 != $SOURCE_LINE2 -o $TARGET_LINE2 != $SOURCE_LINE2 ]
+if [ "$TARGET_LINE1" != "$SOURCE_LINE1" -o "$TARGET_LINE2" != "$SOURCE_LINE2" -o "$TARGET_LINE3" != "$SOURCE_LINE3" ]
 then
-    echo "not equal -> replacing"
     sed -i 's/#!\/bin\/sh//g' $1
 
     cp $1 /tmp/out
