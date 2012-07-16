9ac52894894a33bcd34b3e8a30e102588993d652
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-16 12:39:13 +1000

    Moved the network service back to compute1

--- 07-openstack-controller/25-nova-start.sh
+++ 07-openstack-controller/25-nova-start.sh
@@ -7,7 +7,6 @@ service openstack-nova-consoleauth restart
 service openstack-nova-direct-api restart
 service openstack-nova-metadata-api restart
 service openstack-nova-scheduler restart
-service openstack-nova-network restart
 
 # iSCSI target daemon for nova-volume
 service tgtd restart
@@ -21,6 +20,5 @@ chkconfig openstack-nova-consoleauth on
 chkconfig openstack-nova-direct-api on
 chkconfig openstack-nova-metadata-api on
 chkconfig openstack-nova-scheduler on
-chkconfig openstack-nova-network on
 chkconfig tgtd on
 chkconfig openstack-nova-volume on
--- lib/nova-config.sh
+++ lib/nova-config.sh
@@ -15,7 +15,7 @@ openstack-config --set /etc/nova/api-paste.ini filter:authtoken auth_uri $NOVA_O
 
 
 # Network setup
-openstack-config --set /etc/nova/nova.conf DEFAULT network_host controller
+openstack-config --set /etc/nova/nova.conf DEFAULT network_host compute1
 openstack-config --set /etc/nova/nova.conf DEFAULT fixed_range 10.0.0.0/24
 openstack-config --set /etc/nova/nova.conf DEFAULT flat_interface eth1
 openstack-config --set /etc/nova/nova.conf DEFAULT flat_network_bridge br100
@@ -36,4 +36,4 @@ openstack-config --set /etc/nova/nova.conf DEFAULT xvpvncproxy_base_url http://$
 
 
 # Metadata service
-#openstack-config --set /etc/nova/nova.conf DEFAULT metadata_host $METADATA_HOST
+openstack-config --set /etc/nova/nova.conf DEFAULT metadata_host $METADATA_HOST
