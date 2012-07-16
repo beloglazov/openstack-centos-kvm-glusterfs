41bc2d9243a4eea976acadd41f23e8bbafcdca16
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-16 19:15:29 +1000

    Added the license header to all the shell scripts

--- 09-openstack-gateway/03-nova-network-create.sh
+++ 09-openstack-gateway/03-nova-network-create.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
+# Copyright 2012 Anton Beloglazov
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 # Create a Nova network for VM instances: 10.0.0.0/24
 nova-manage network create --label=public --fixed_range_v4=10.0.0.0/24 --num_networks=1 --network_size=256 --bridge=br100
