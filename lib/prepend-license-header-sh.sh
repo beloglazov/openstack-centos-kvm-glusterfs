db4de832b7ecccfdd3886f03cf8498218a6a4ba6
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-16 19:09:04 +1000

    Updated the license prepend script for handling multiple files

--- lib/prepend-license-header-sh.sh
+++ lib/prepend-license-header-sh.sh
@@ -4,30 +4,33 @@
 # license-header file to the file specified as an argument if the
 # header has not yet been prepended.
 
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-    echo "You must specify one argument - the file to prepend the license header to"
+    echo "You must specify one or more arguments - the files to prepend the license header to"
     exit 1
 fi
 
+for TARGET in "$@"
+do
+    DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    SOURCE=${DIR}/license-header-sh
 
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SOURCE=${DIR}/license-header-sh
+    TARGET_LINE1=`cat $TARGET | sed '1!d;q'`
+    TARGET_LINE2=`cat $TARGET | sed '2!d;q'`
+    TARGET_LINE3=`cat $TARGET | sed '3!d;q'`
 
-TARGET_LINE1=`cat $1 | sed '1!d;q'`
-TARGET_LINE2=`cat $1 | sed '2!d;q'`
-TARGET_LINE3=`cat $1 | sed '3!d;q'`
+    SOURCE_LINE1=`cat $SOURCE | sed '1!d;q'`
+    SOURCE_LINE2=`cat $SOURCE | sed '2!d;q'`
+    SOURCE_LINE3=`cat $SOURCE | sed '3!d;q'`
 
-SOURCE_LINE1=`cat $SOURCE | sed '1!d;q'`
-SOURCE_LINE2=`cat $SOURCE | sed '2!d;q'`
-SOURCE_LINE3=`cat $SOURCE | sed '3!d;q'`
+    if [ "$TARGET_LINE1" != "$SOURCE_LINE1" -o "$TARGET_LINE2" != "$SOURCE_LINE2" -o "$TARGET_LINE3" != "$SOURCE_LINE3" ]
+    then
+	sed -i 's/#!\/bin\/sh//g' $TARGET
 
-if [ "$TARGET_LINE1" != "$SOURCE_LINE1" -o "$TARGET_LINE2" != "$SOURCE_LINE2" -o "$TARGET_LINE3" != "$SOURCE_LINE3" ]
-then
-    sed -i 's/#!\/bin\/sh//g' $1
+	cp $TARGET /tmp/out
+	cat $SOURCE > $TARGET
+	cat /tmp/out >> $TARGET
+	rm -f /tmp/out
+    fi
+done
 
-    cp $1 /tmp/out
-    cat $SOURCE > $1
-    cat /tmp/out >> $1
-    rm -f /tmp/out
-fi
