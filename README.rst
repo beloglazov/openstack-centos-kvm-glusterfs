dc54480bbc3cf9dcd8eb4d8292d7101d6fe08655
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-07-23 17:24:14 +1000

    Moved the rst version to README.rst

--- README.rst
+++ README.rst
@@ -149,7 +149,7 @@ paper with a summary and discussion of future directions.
 Overview of the OpenStack Cloud Platform
 ========================================
 
-.. figure:: ../../../raw/master/doc/src/openstack-software-diagram.png
+.. figure:: ../../raw/master/doc/src/openstack-software-diagram.png
    :align: center
    :alt: A high level view of the OpenStack service interaction [3]
 
--- doc/src/build-rst.sh
+++ doc/src/build-rst.sh
@@ -3,10 +3,10 @@
 pandoc \
     --smart \
     --bibliography=bibliography.bib --csl=ieee.csl \
-    --output=../openstack-centos-kvm-glusterfs-guide.rst \
+    --output=../../README.rst \
     readme-intro.md \
     abstract.txt \
     openstack-centos-kvm-glusterfs-guide.md
 
-sed -i 's/openstack-software-diagram.png/..\/..\/..\/raw\/master\/doc\/src\/openstack-software-diagram.png/g' \
-    ../openstack-centos-kvm-glusterfs-guide.rst
+sed -i 's/openstack-software-diagram.png/..\/..\/raw\/master\/doc\/src\/openstack-software-diagram.png/g' \
+    ../../README.rst
