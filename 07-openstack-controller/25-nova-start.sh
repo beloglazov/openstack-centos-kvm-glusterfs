7b25d9ddbc93717b04d6312d2d214ecb3fa4efff
Anton Beloglazov <anton.beloglazov@gmail.com>
2012-12-17 12:21:02 +1100

    Commented out openstack-nova-direct-api, as it has been removed in Folsom

--- 07-openstack-controller/25-nova-start.sh
+++ 07-openstack-controller/25-nova-start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # Copyright 2012 Anton Beloglazov
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,9 @@ service tgtd restart
 service openstack-nova-api restart
 service openstack-nova-cert restart
 service openstack-nova-consoleauth restart
-service openstack-nova-direct-api restart
+# The following has been removed in Folsom
+# Uncomment for older versions of OpenStack
+#service openstack-nova-direct-api restart
 service openstack-nova-metadata-api restart
 service openstack-nova-scheduler restart
 service openstack-nova-volume restart
@@ -36,7 +38,9 @@ chkconfig tgtd on
 chkconfig openstack-nova-api on
 chkconfig openstack-nova-cert on
 chkconfig openstack-nova-consoleauth on
-chkconfig openstack-nova-direct-api on
+# The following has been removed in Folsom
+# Uncomment for older versions of OpenStack
+#chkconfig openstack-nova-direct-api on
 chkconfig openstack-nova-metadata-api on
 chkconfig openstack-nova-scheduler on
 chkconfig openstack-nova-volume on
